a2d039390f5b7604e5331a5432920b8771483265
Do Vuong <vuong.trong.do@aioz.io>
2019-12-30 10:48:40 +0700

    hello express

--- myApp.js
+++ myApp.js
@@ -9,10 +9,12 @@ var app = express();
 
 
 /** 1) Meet the node console. */
-
+console.log("Hello World");
 
 /** 2) A first working Express Server */
-
+app.get("/", function(req, res) {
+    return res.send("Hello Express");
+});
 
 /** 3) Serve an HTML file */
 
